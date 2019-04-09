fce31e391a14a579a1da0b0943b29097d20cf216
Chris Nero <chrisvn00@gmail.com>
2019-04-09 13:05:37 -0400

    get_data, do not extract yet

--- mvn.py
+++ mvn.py
@@ -1,6 +1,18 @@
 import urllib.request as req
 import os.path
+import re
 
+
+mvn_pattern = re.compile(
+    r"""
+        \[
+        (?P<group>[^:]+):
+        (?P<artifact>[^:]+):
+        (?P<version>[^:]+)
+        (?::(?P<classifier>\w+))?
+        (?:@(?P<ext>\w+))?
+        \]
+    """, re.X)
 
 def get_libraries(install_profile, root):
     libs_prof = install_profile['libraries']
@@ -18,3 +30,31 @@ def get_libraries(install_profile, root):
                     with req.urlopen(url) as data:
                         f.write(data.read())
     return libs
+
+def get_data(install_profile, root):
+    data_prof = install_profile['data']
+    data = {}
+    for datum_name in data_prof:
+        path_spec = data_prof[datum_name]['client']
+        if path_spec[0] == '[':
+            # maven specifiers are in square brackets
+            data[datum_name] = os.path.join(root, maven_to_path(path_spec))
+            pass
+        elif path_spec[0] == "'":
+            # we don't use the SHA hashes
+            pass
+        else:
+            # literal path, extract to new location
+            data[datum_name] = os.path.join(root, path_spec[1:])
+    return data
+
+def maven_to_path(path_spec):
+    match = mvn_pattern.search(path_spec)
+    return "{group}/{artifact}/{version}/{artifact}-{version}{d}{classifier}.{ext}".format(
+            group=match['group'].replace('.', '/'),
+            artifact=match['artifact'],
+            version=match['version'],
+            d='-' if match['classifier'] else '',
+            classifier=match['classifier'] or '',
+            ext=match['ext'] or 'jar',
+        )
